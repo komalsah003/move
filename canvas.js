f05b4d475a800859320b44c07e5c133a7feb897e
komal kumari <78696212+komalsah003@users.noreply.github.com>
2021-05-10 11:23:10 +0530

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,19 +1,14 @@
 var canvas = document.querySelector('canvas')
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 var c = canvas.getContext('2d');
 var x = 10;
 function animate() {
     requestAnimationFrame(animate);
-    //  c.clearRect();
     c.beginPath();
     c.arc(x, 275, 30, 0, Math.PI * 2, false);
     c.strokeStyle = 'blue';
     c.stroke();
     x += 1;
-
 }
-
 animate();
